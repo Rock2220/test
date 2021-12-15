d544fe97b392ca205fde952261039bc85df49659
zTurnerr <edwardnewgate7676@gmail.com>
2021-12-15 13:20:55 +0200

    15-DEC

---
+++ VueNPM/store/src/components/TestWelcome.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="testPage">
+        <h3>Welcome Page</h3>
+        <WelcomeMessage />
+    </div>
+</template>
+
+<script>
+export default {
+    name: "WelcomeMessage",
+};
+</script>
+
+<style>
+h2 {
+    font-size: larger;
+}
+</style>
